e87f0b3dac33ef2aa4be3b7760d81adc73f7bad2
Alejandro Torres Naranjo <124210642+i2001atnj@users.noreply.github.com>
2023-08-05 10:21:56 -0500

    changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './styles/App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Homepage from './pages/Homepage';
+import SearchPage from './pages/SearchPage';
 
 function App() {
   return (
     <div className="App">
-      <Homepage/>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Homepage/>}/>
+          <Route path="/search" element={<SearchPage/>}/>
+        </Routes>
+      </Router>
     </div>
   );
 }
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
 import '../styles/Homepage.css'
+import { Link } from 'react-router-dom'
+import Dots from '../assets/Vector.svg'
+import Photo from '../assets/profilePhoto.png'
 
 function Homepage() {
   return (
     <div className='homepage'>
       <div className="homepage__header">
-
+        <Link className='homepage__header-link' to='https://mail.google.com/mail/u/0/#inbox'>Gmail</Link>
+        <Link className='homepage__header-link' to='https://www.google.com/imghp?hl=en&ogbl'>Images</Link>
+        <button className='homepage__button-apps'>
+          <img style={{height: 16}} src={Dots} alt='Dots'/>
+          <img style={{height: 16}} src={Dots} alt='Dots'/>
+          <img style={{height: 16}} src={Dots} alt='Dots'/>
+        </button>
+        <button className='homepage__button-profile'>
+          <img style={{height: 32, borderRadius: 50}} src={Photo} alt="ProfilePhoto" />
+        </button>
       </div>
       <div className="homepage__body">
         <ul>
