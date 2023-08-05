e4934a50c981f5f9a009fb06da1412380f171f7e
Alejandro Torres Naranjo <124210642+i2001atnj@users.noreply.github.com>
2023-08-04 19:46:18 -0500

    changes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import './App.css';
+import './styles/App.css';
+import Homepage from './pages/Homepage';
 
 function App() {
   return (
     <div className="App">
+      <Homepage/>
     </div>
   );
 }
---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import '../styles/Homepage.css'
+
+function Homepage() {
+  return (
+    <div className='homepage'>
+      <div className="homepage__header">
+
+      </div>
+      <div className="homepage__body">
+        <ul>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+export default Homepage
