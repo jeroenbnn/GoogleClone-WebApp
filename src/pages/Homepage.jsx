6f4b0714bb0d279cf488cad3f9a327b66d08642b
Alejandro Torres Naranjo <124210642+i2001atnj@users.noreply.github.com>
2023-08-07 11:40:32 -0500

    changes

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,10 +1,8 @@
 import React from "react";
 import "../styles/Homepage.css";
 import { Link } from "react-router-dom";
-import Dots from "../assets/Vector.svg";
-import Photo from "../assets/profilePhoto.png";
 import MainLogo from "../assets/MainLogo.svg";
-import Search from "../components/Search";
+import Searchbar from "../components/Searchbar";
 
 function Homepage() {
   return (
@@ -12,23 +10,11 @@ function Homepage() {
       <div className="homepage__header">
         <Link to="https://mail.google.com/mail/u/0/#inbox">Gmail</Link>
         <Link to="https://www.google.com/imghp?hl=en&ogbl">Images</Link>
-        <button className="homepage__button-apps">
-          <img style={{ height: 16 }} src={Dots} alt="Dots" />
-          <img style={{ height: 16 }} src={Dots} alt="Dots" />
-          <img style={{ height: 16 }} src={Dots} alt="Dots" />
-        </button>
-        <button className="homepage__button-profile">
-          <img
-            style={{ height: 32, borderRadius: 50 }}
-            src={Photo}
-            alt="ProfilePhoto"
-          />
-        </button>
       </div>
       <div className="homepage__body">
         <img style={{ height: 92, width: 272 }} src={MainLogo} alt="Logo" />
         <div className="homepage__inputContainer">
-          <Search />
+          <Searchbar />
         </div>
       </div>
     </div>
--- src/pages/SearchPage.jsx
+++ src/pages/SearchPage.jsx
@@ -1,20 +1,16 @@
 import React from "react";
 import "../styles/SearchPage.css";
 import { useStateValue } from "../context";
-// import useGoogleSearch from "../useGoogleSearch";
-import Response from "../response";
+import useGoogleSearch from "../useGoogleSearch";
 import Logo from "../assets/MainLogo.svg";
 import { Link } from "react-router-dom";
-import Search from "../components/Search";
+import Searchbar from "../components/Searchbar";
 import SettingsIcon from "../assets/icon.png";
-import Dots from "../assets/Vector.svg";
 import "../styles/SearchPage.css";
-import Photo from "../assets/profilePhoto.png";
 
 function SearchPage() {
-  const [{ term }, dispatch] = useStateValue();
-  // const { data } = useGoogleSearch(term);
-  const data = Response;
+  const [{ term }] = useStateValue();
+  const { data } = useGoogleSearch(term);
   return (
     <div className="searchpage">
       <div className="searchpage__header">
@@ -28,23 +24,11 @@ function SearchPage() {
                 alt="Logo"
               />
             </Link>
-            <Search hideButtons />
+            <Searchbar hideButtons />
           </div>
           <div className="searchpage__topRightButtons">
             <button className="searchpage__settingsButton">
               <img src={SettingsIcon} alt="" />
-            </button>
-            <button className="searchpage__appsButton">
-              <img style={{ height: 16 }} src={Dots} alt="Dots" />
-              <img style={{ height: 16 }} src={Dots} alt="Dots" />
-              <img style={{ height: 16 }} src={Dots} alt="Dots" />
-            </button>
-            <button className="homepage__button-profile">
-              <img
-                style={{ height: 32, borderRadius: 50 }}
-                src={Photo}
-                alt="ProfilePhoto"
-              />
             </button>
           </div>
         </div>
@@ -116,6 +100,72 @@ function SearchPage() {
             </div>
           </div>
         </div>
+      </div>
+      <div key={Math.random()}>
+        {term && (
+          <div style={{ color: "white" }} className="searchpage__results">
+            <p className="searchpage__resultsCount">
+              About {data?.searchInformation.formattedTotalResults} results (
+              {data?.searchInformation.formattedSearchTime} seconds) for {term}
+            </p>
+            {data?.items.map((item) => (
+              <div key={item.cacheId} className="searchpage__result">
+                <a
+                  href={item.link}
+                  style={{
+                    color: "#bdc1c6",
+                    fontSize: 13,
+                    fontWeight: 500,
+                    display: "flex",
+                    gap: "10px",
+                  }}
+                >
+                  {item.pagemap?.cse_image?.length > 0 &&
+                    item.pagemap?.cse_image[0]?.src && (
+                      <div
+                        style={{
+                          backgroundColor: "#fff",
+                          height: 30,
+                          width: 30,
+                          borderRadius: 50,
+                          display: "flex",
+                          justifyContent: "center",
+                          alignItems: "center",
+                        }}
+                      >
+                        <img
+                          className="searchpage__resultImage"
+                          src={item.pagemap?.cse_image[0]?.src}
+                          alt=""
+                        />
+                      </div>
+                    )}
+                  <div
+                    style={{
+                      display: "flex",
+                      flexDirection: "column",
+                      justifyContent: "center",
+                    }}
+                  >
+                    <p style={{ fontSize: 14, color: "#dadce0" }}>
+                      {item.pagemap?.metatags[0]["og:title"]}
+                    </p>
+                    <p style={{ fontSize: 12 }}>{item.displayLink}</p>
+                  </div>
+                </a>
+                <a href={item.link}>
+                  <h2
+                    className="searchpage__resultTitle"
+                    style={{ fontSize: 20, fontWeight: 500 }}
+                  >
+                    {item.title}
+                  </h2>
+                </a>
+                <p style={{ color: "#bdc1c6", fontSize: 13 }}>{item.snippet}</p>
+              </div>
+            ))}
+          </div>
+        )}
       </div>
     </div>
   );
